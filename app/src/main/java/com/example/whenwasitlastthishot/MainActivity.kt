e70f2cfeb1d475cab213898867bab0baf9afc629
Simon <coopersimon@gmail.com>
2020-09-09 22:09:28 +0100

    raw data json

--- app/src/main/java/com/example/whenwasitlastthishot/MainActivity.kt
+++ app/src/main/java/com/example/whenwasitlastthishot/MainActivity.kt
@@ -1,17 +1,17 @@
 package com.example.whenwasitlastthishot
 
+import android.content.res.Resources
 import android.os.Bundle
 import android.util.Log
-import android.view.inputmethod.EditorInfo
-import android.widget.EditText
-import android.widget.TextView.OnEditorActionListener
 import androidx.appcompat.app.AppCompatActivity
 import androidx.viewpager.widget.ViewPager
 import com.example.whenwasitlastthishot.ui.main.SectionsPagerAdapter
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.tabs.TabLayout
-import java.util.*
+import org.json.JSONArray
+import java.io.BufferedReader
+import java.io.InputStreamReader
 
 
 class MainActivity : AppCompatActivity() {
@@ -33,6 +33,16 @@ class MainActivity : AppCompatActivity() {
         }
 
         val asection = sectionsPagerAdapter.getItem(0)
+
+        //val k = resources.getStringArray(R.array.planets_array)
+        //val json = "[[\"a\",\"b\",\"c\"],\"d\"]"
+        val rawResource  = getResources().openRawResource(R.raw.temperature);
+        val r = BufferedReader(InputStreamReader(rawResource))
+        val allText = rawResource.bufferedReader().use(BufferedReader::readText)
+
+        val obj = JSONArray(allText)
+        Log.d("MyLog", obj.toString());
+        Log.d("MyLog", (obj[0] as JSONArray)[2].toString());
 
         //Log.i("myLog", foos.toString())
         val even: Array<Array<String?>> = arrayOf(arrayOf("2", "4"), arrayOf("2", "4"))
